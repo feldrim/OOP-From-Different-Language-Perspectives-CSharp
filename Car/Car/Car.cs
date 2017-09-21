bf60bc0ed2048825334b3bf71fb0559b69badf8c
the f <feldrim@gmail.com>
2017-09-22 00:17:47 +0300

    Auto property kullanıldı.

--- Car/Car/Car.cs
+++ Car/Car/Car.cs
@@ -4,56 +4,36 @@ namespace car
 {
 	public class Car
 	{
-	    private string _make;
-	    private string _model;
-	    private string _year;
-	    private int _speed;
-	    private int _distance;
+        public string Make { get; set; }
 
-		public void go (int newDistance)
+	    public string Model { get; set; }
+
+	    public string Year { get; set; }
+
+	    public int Speed { get; set; }
+
+	    public int Distance { get; set; }
+
+        public void go (int newDistance)
 		{
-			_distance += newDistance;
+			Distance += newDistance;
 		}
 
 		public void accelerate (int newSpeed)
 		{
-			_speed = newSpeed;
+			Speed = newSpeed;
 		}
 
 		public void stop ()
 		{
-			_speed = 0;
+			Speed = 0;
 		}
 
 		public string getInfo ()
 		{
-			return "Car Info: " + _year + " " + _make + " " + _model + ". Distance: " + _distance + " km. and traveling at " + _speed + " kmph.";
+			return "Car Info: " + Year + " " + Make + " " + Model + ". Distance: " + Distance + " km. and traveling at " + Speed + " kmph.";
 		}
 
-		public string Make {
-			get { return _make; }
-			set { _make = value; }
-		}
-
-		public string Model {
-			get { return _model; }
-			set { _model = value; }
-		}
-
-		public string Year {
-			get { return _year; }
-			set { _year = value; }
-		}
-
-		public int Speed {
-			get { return _speed; }
-			set { _speed = value; }
-		}
-
-		public int Distance {
-			get { return _distance; }
-			set { _distance = value; }
-		}
 
 	}
 }
