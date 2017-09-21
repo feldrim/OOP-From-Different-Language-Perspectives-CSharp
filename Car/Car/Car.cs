64cdbe124084b769f647f164ee6e897b9e4e5187
the f <feldrim@gmail.com>
2017-09-22 00:16:57 +0300

    Property'lerin access modifier'ı private olarak düzenlendi. isimlendirme underscore ile yapıldı.

--- Car/Car/Car.cs
+++ Car/Car/Car.cs
@@ -4,55 +4,55 @@ namespace car
 {
 	public class Car
 	{
-		string make;
-		string model;
-		string year;
-		int speed;
-		int distance;
+	    private string _make;
+	    private string _model;
+	    private string _year;
+	    private int _speed;
+	    private int _distance;
 
 		public void go (int newDistance)
 		{
-			distance += newDistance;
+			_distance += newDistance;
 		}
 
 		public void accelerate (int newSpeed)
 		{
-			speed = newSpeed;
+			_speed = newSpeed;
 		}
 
 		public void stop ()
 		{
-			speed = 0;
+			_speed = 0;
 		}
 
 		public string getInfo ()
 		{
-			return "Car Info: " + year + " " + make + " " + model + ". Distance: " + distance + " km. and traveling at " + speed + " kmph.";
+			return "Car Info: " + _year + " " + _make + " " + _model + ". Distance: " + _distance + " km. and traveling at " + _speed + " kmph.";
 		}
 
 		public string Make {
-			get { return make; }
-			set { make = value; }
+			get { return _make; }
+			set { _make = value; }
 		}
 
 		public string Model {
-			get { return model; }
-			set { model = value; }
+			get { return _model; }
+			set { _model = value; }
 		}
 
 		public string Year {
-			get { return year; }
-			set { year = value; }
+			get { return _year; }
+			set { _year = value; }
 		}
 
 		public int Speed {
-			get { return speed; }
-			set { speed = value; }
+			get { return _speed; }
+			set { _speed = value; }
 		}
 
 		public int Distance {
-			get { return distance; }
-			set { distance = value; }
+			get { return _distance; }
+			set { _distance = value; }
 		}
 
 	}
