4ff18f3ff1f9d63e00c7b52a968d1f1e00239ce1
the f <feldrim@gmail.com>
2017-09-22 00:23:36 +0300

    Object initializer kullanıldı.

--- Car/Car/CarTest.cs
+++ Car/Car/CarTest.cs
@@ -11,13 +11,15 @@ namespace Car
 		public static void Main (string [] args)
 		{
 
-			var car1 = new Car ();
-			car1.Make = "Mercedes";
-			car1.Model = "C200";
-			car1.Year = "2017";
-			car1.Distance = 0;
-			car1.Speed = 0;
-			Console.WriteLine (car1.GetInfo ());
+            var car1 = new Car
+            {
+                Make = "Mercedes",
+                Model = "C200",
+                Year = "2017",
+                Distance = 0,
+                Speed = 0
+            };
+            Console.WriteLine (car1.GetInfo ());
 
 			car1.Accelerate (125);
 			car1.Go (100);
@@ -26,13 +28,15 @@ namespace Car
 
 			Console.WriteLine ();
 
-			var car2 = new Car ();
-			car2.Make = "BMW";
-			car2.Model = "i8";
-			car2.Year = "2016";
-			car2.Distance = 10000;
-			car2.Speed = 0;
-			Console.WriteLine (car2.GetInfo ());
+		    var car2 = new Car
+		    {
+		        Make = "BMW",
+		        Model = "i8",
+		        Year = "2016",
+		        Distance = 10000,
+		        Speed = 0
+		    };
+		    Console.WriteLine (car2.GetInfo ());
 
 			car2.Accelerate (220);
 			car2.Go (150);
